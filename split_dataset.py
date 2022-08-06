955f352773dff3e3ea9404557b14003641be355c
lots-o <kgh0916@gmail.com>
2022-08-06 15:47:38 +0900

    modify train/valid/test dataset

--- split_dataset.py
+++ split_dataset.py
@@ -25,15 +25,11 @@ anomal_test = np.array([p.extract_url(request) for request in anomal_test])
 
 logger.info("Split dataset")
 normal_valid, normal_test = train_test_split(
-    normal_train, train_size=0.5, test_size=0.5, random_state=42, shuffle=True
-)  # normalTrafficTraining -> 50%, normal validation
-anomal_valid, anomal_test = train_test_split(
-    anomal_test, train_size=0.5, test_size=0.5, random_state=42, shuffle=True
-)  # anomalTrafficTest -> 50%, anomalTrafficValid
+    normal_test, train_size=0.3, test_size=0.7, random_state=42, shuffle=True
+)  # normalTrafficTest () -> 30%, normal validation
 
 logger.info("Save dataset")
 np.savetxt(os.path.join(save_dir, "normalTrafficTraining.txt"), normal_train, fmt="%s")
 np.savetxt(os.path.join(save_dir, "normalTrafficValid.txt"), normal_valid, fmt="%s")
 np.savetxt(os.path.join(save_dir, "normalTrafficTest.txt"), normal_test, fmt="%s")
-np.savetxt(os.path.join(save_dir, "anomalousTrafficValid.txt"), anomal_valid, fmt="%s")
 np.savetxt(os.path.join(save_dir, "anomalousTrafficTest.txt"), anomal_test, fmt="%s")
